8e6fa07cc046435c4f1fed07190aba565c0d023f
Jose Pereira <jose.pereira@wizeline.com>
2021-06-07 00:52:56 -0500

    feat(base-web-app): adding compression at prod build

--- packages/base-web-app/vue.config.js
+++ packages/base-web-app/vue.config.js
@@ -1,7 +1,24 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
+const BundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
 
-  productionSourceMap: false
-}
+const compresionPlugin = new CompressionPlugin({
+  algorithm: 'gzip',
+  test: /\.(js|css)$/,
+  deleteOriginalAssets: true,
+  filename: '[path][base]',
+});
+
+const bundleAnalyzerPlugin = new BundleAnalyzer();
+
+module.exports = {
+  transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch');
+    if (process.env.NODE_ENV == 'production') {
+      config.plugin('CompressionPlugin').use(compresionPlugin);
+    } else {
+      config.plugin('BundleAnalyzer').use(bundleAnalyzerPlugin);
+    }
+  },
+  productionSourceMap: false,
+};
